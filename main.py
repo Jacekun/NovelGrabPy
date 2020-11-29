2291cd28a8d836100498dfc1780d97f66a587014
Jace <54625750+Jacekun@users.noreply.github.com>
2020-11-29 17:51:52 +0800

    Scrape page

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # imports
 import os
 import imp
+import requests
+from bs4 import BeautifulSoup
 
 # define our clear function 
 def clear(): 
@@ -23,3 +25,19 @@ class Main:
     modInit = getattr( module, "extInfo" )
 
     print( modInit("Package Info:\n") )
+
+    # Get webnovel url
+    wnPage = input("Paste base URL here: ")
+    page = requests.get(wnPage)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(id='chapters_1-0')
+
+    chapters = results.find('ul', class_='chapter-chs')
+    chaptersList = chapters.find_all('a', class_='')
+
+    for ch in chaptersList:
+        if ch.has_attr('href'):
+            print(ch.text + " : " + ch['href'])
+
+    print("Done!")
